ce20329d74ee26e4844adaae3e3ea3b0bccbadde
Filipe Varjão <varjaofilipe@gmail.com>
2024-07-02 17:25:46 +0100

    force fetch ex_webrtc deps "mix deps.update --all --force"

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Tchain.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ex_webrtc, "~> 0.3.0"}
     ]
   end
 
