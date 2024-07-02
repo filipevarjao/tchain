27693a00ef1b0122779e53022a95f34a0a57a394
Filipe Varjão <varjaofilipe@gmail.com>
2024-07-02 16:38:27 +0100

    first commit

---
+++ lib/tchain/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Tchain.Mailer do
+  use Swoosh.Mailer, otp_app: :tchain
+end
---
+++ test/tchain_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule TchainWeb.PageControllerTest do
+  use TchainWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
